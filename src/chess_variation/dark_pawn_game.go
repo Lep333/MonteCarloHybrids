06e35cf9c6509c32f6a3d02b3304b5030dedb960
leo lo <leo.pirker@lopis.de>
2025-10-24 17:26:47 +0200

    Changed dark pawn chess board to have flexible board size.

--- src/chess_variation/dark_pawn_game.go
+++ src/chess_variation/dark_pawn_game.go
@@ -4,31 +4,31 @@ import (
 	"math"
 )
 
-const row_length = 5
-const no_fields = row_length * row_length
-const black_base_line_start = row_length * (row_length - 1)
+const row_length uint = 8
+const no_fields uint = row_length * row_length
+const black_base_line_start uint = row_length * (row_length - 1)
+const row_bitmask uint = (1 << row_length) - 1
 
 type DarkPawnChess struct {
-	white_pawns         int
-	white_pawns_moves   [no_fields]int
-	white_pawns_capture [no_fields]int
-	black_pawns         int
-	black_pawns_moves   [no_fields]int
-	black_pawns_capture [no_fields]int
+	white_pawns         uint
+	white_pawns_moves   [no_fields]uint
+	white_pawns_capture [no_fields]uint
+	black_pawns         uint
+	black_pawns_moves   [no_fields]uint
+	black_pawns_capture [no_fields]uint
 }
 
 func (d *DarkPawnChess) InitGame() {
-	row_bitmask := 0b11111
 	d.white_pawns = row_bitmask
-	d.black_pawns = row_bitmask << 20
+	d.black_pawns = row_bitmask << black_base_line_start
 
-	for i := 0; i < no_fields; i++ {
+	for i := uint(0); i < no_fields; i++ {
 		if i < no_fields-row_length {
 			d.white_pawns_moves[i] = 0b1 << (i + row_length)
-			if i-4%5 != 0 {
+			if i-row_length+1%row_length != 0 {
 				d.white_pawns_capture[i] = 0b1 << (i + row_length + 1) // left capture
 			}
-			if i%5 != 0 {
+			if i%row_length != 0 {
 				d.white_pawns_capture[i] = 0b1 << (i + row_length - 1) // right capture
 			}
 		}
@@ -36,10 +36,10 @@ func (d *DarkPawnChess) InitGame() {
 		if i >= row_length {
 			d.black_pawns_moves[i] = 0b1 << (i - row_length)
 
-			if i-4%5 != 0 {
+			if i-row_length+1%row_length != 0 {
 				d.black_pawns_capture[i] = 0b1 << (i - row_length + 1) // left capture
 			}
-			if i%5 != 0 {
+			if i%row_length != 0 {
 				d.black_pawns_capture[i] = 0b1 << (i - row_length - 1) // right capture
 			}
 		}
@@ -52,10 +52,9 @@ func (d *DarkPawnChess) ReturnBoard() ChessVariation {
 
 func (d *DarkPawnChess) GameOver() bool {
 	game_over := false
-	row_bitmask := 0b11111
 
 	// check for opposing pawns on base line
-	for i := 0; i < row_length; i++ {
+	for i := uint(0); i < row_length; i++ {
 		if d.black_pawns&row_bitmask > 0 {
 			game_over = true
 		}
@@ -70,7 +69,7 @@ func (d *DarkPawnChess) GameOver() bool {
 func (d *DarkPawnChess) PossibleMoves(whiteToPlay bool) []Move {
 	moves := []Move{}
 
-	for i := 0; i < no_fields; i++ {
+	for i := uint(0); i < no_fields; i++ {
 		if whiteToPlay && d.white_pawns&(0b1<<i) > 0 {
 			move_to_possible := d.white_pawns_moves[i] & ^d.black_pawns
 			if move_to_possible > 0 {
@@ -79,7 +78,7 @@ func (d *DarkPawnChess) PossibleMoves(whiteToPlay bool) []Move {
 			}
 
 			capture_possible := d.white_pawns_capture[i] & d.black_pawns
-			for position := 0; position < no_fields; position++ {
+			for position := uint(0); position < no_fields; position++ {
 				if (capture_possible>>position)&0b1 != 0 {
 					move := Move{int8(i), int8(position)}
 					moves = append(moves, move)
@@ -95,7 +94,7 @@ func (d *DarkPawnChess) PossibleMoves(whiteToPlay bool) []Move {
 			}
 
 			capture_possible := d.black_pawns_capture[i] & d.white_pawns
-			for position := 0; position < no_fields; position++ {
+			for position := uint(0); position < no_fields; position++ {
 				if (capture_possible>>position)&0b1 != 0 {
 					move := Move{int8(i), int8(position)}
 					moves = append(moves, move)
@@ -107,9 +106,9 @@ func (d *DarkPawnChess) PossibleMoves(whiteToPlay bool) []Move {
 }
 
 func (d *DarkPawnChess) ExecuteMove(whiteToPlay bool, move Move) {
-	mask_from := 1 << move.from
-	mask_to := 1 << move.to
-	mask := mask_from | mask_to
+	var mask_from uint = 1 << move.from
+	var mask_to uint = 1 << move.to
+	var mask uint = mask_from | mask_to
 	if whiteToPlay {
 		d.white_pawns ^= mask
 		d.black_pawns = d.black_pawns &^ mask_to
@@ -121,21 +120,17 @@ func (d *DarkPawnChess) ExecuteMove(whiteToPlay bool, move Move) {
 
 func (d *DarkPawnChess) String() string {
 	board := ""
-	for i := no_fields - 1; i >= 0; i-- {
+	for i := int(no_fields - 1); i >= 0; i-- {
 		if d.white_pawns&(0b1<<i) != 0 {
 			board += "w "
 		} else if d.black_pawns&(0b1<<i) != 0 {
 			board += "b "
 		} else {
-			board += "  "
+			board += "o "
 		}
-		if i%5 == 0 {
+		if uint(i)%row_length == 0 {
 			board += "\n"
 		}
 	}
 	return board
-}
-
-func realize() {
-
 }
